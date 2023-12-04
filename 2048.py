1147cad988d1b136b970eced7a7f4de419e2b6f3
Kaktousse <147042560+Kaktousse@users.noreply.github.com>
2023-12-04 14:32:55 +0100

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -3,7 +3,7 @@ import keyboard
 import random
 
 def Refresh(grid : list[list[str]]):
-    print("\n"*100)
+    #print("\n"*100)
     Drawgrid(grid)
 
 def Creategrid(lenght:int) -> list[list[str]]:
@@ -27,19 +27,28 @@ def Drawgrid(grid: list[list[str]]):
         print("\t", "-"*(4*word_length))
 
 def Game(grid: list[list[str]]):
-    Refresh(grid)
     while True:
-        if keyboard.is_pressed("left arrow"):
-            ax = "lign" ; dir = -1
-        elif keyboard.is_pressed("right arrow"):
-            ax = "lign" ; dir = 1
-        elif keyboard.is_pressed("down arrow"):
-            ax = "column" ; dir = 1
-        elif keyboard.is_pressed("up arrow"):
-            ax = "column" ; dir = -1
-        grid = Moving(ax,dir)
-        grid = Addnum(grid)
-        Refresh(grid)
+        k = input(' : ')
+        if k == 'q':
+            print("left")
+            grid = Moving('left',grid)
+            grid = Addnum(grid)
+            Refresh(grid)
+        elif k == 'd':
+            print("right")
+            grid = Moving('right',grid)
+            grid = Addnum(grid)
+            Refresh(grid)  
+        elif k == 'z':
+            print("up")
+            grid = Moving('up',grid)
+            grid = Addnum(grid)
+            Refresh(grid)  
+        elif k == 's':
+            print("down")
+            grid = Moving('down',grid)
+            grid = Addnum(grid)
+            Refresh(grid)       
 
 def GetAvailableTiles(grid : list[list[str]]) -> list[tuple[int, int]]:
     Availabletiles :list  = []
@@ -50,58 +59,81 @@ def GetAvailableTiles(grid : list[list[str]]) -> list[tuple[int, int]]:
                 Availabletiles.append([lign,column])
     return Availabletiles
 
-def Addnum(grid:list[list[str]]):
+def Addnum(grid:list[list[str]]) -> list[list[str]]:
     availabletiles = GetAvailableTiles(grid)
     for i in range(2):
-        randomindex1 = random.randint(0,len(availabletiles)-1)
-        randomtile = availabletiles[randomindex1]
-        grid[randomtile[0]][randomtile[1]] == random.randint()
+        randomindex = random.randint(0,len(availabletiles)-1)
+        randomtile = availabletiles[randomindex]
+        grid[randomtile[0]][randomtile[1]] = str(random.choice([2,4]))
+        availabletiles.pop(randomindex)
+    return grid
     
     
 
-def  Moving(axis: str,direction: int,grid: list[list[str]]) -> list[list[str]]:
+def  Moving(axis: str,grid: list[list[str]]) -> list[list[str]]:
     new_grid = Creategrid(len(grid))
-
-    if axis == "lign":
-        if direction == 1:
-
+    already_merge = []
+    if axis == "right":
+            
             for lign in range(len(grid)):
-                for column in range(len(grid)):
+                for column in range(len(grid)-1,-1,-1):
                     if grid[lign][column] != " ":
-                        for count in range(len(new_grid),0):
+                        for count in range(len(new_grid)-1,-1,-1):
                             if new_grid[lign][count] == " ":
-                                new_grid[lign][count] == grid[lign][column]
+                                new_grid[lign][count] = grid[lign][column]
+                                grid[lign][column] = " "
                                 break
-
-        elif direction == -1:
-
+                            elif new_grid[lign][count] == grid[lign][column] and [lign,count] not in already_merge:
+                                new_grid[lign][count] = str(int(grid[lign][column]) + int(new_grid[lign][count]))
+                                grid[lign][column] = " "
+                                break
+                               
+    elif axis == "left":
+                                        
             for lign in range(len(grid)):
                 for column in range(len(grid)):
                     if grid[lign][column] != " ":
                         for count in range(len(new_grid)):
                             if new_grid[lign][count] == " ":
-                                new_grid[lign][count] == grid[lign][column]
+                                new_grid[lign][count] = grid[lign][column]
+                                grid[lign][column] = " "
+                                break
+                            elif new_grid[lign][count] == grid[lign][column] and [lign,count] not in already_merge:
+                                new_grid[lign][count] = str(int(grid[lign][column]) + int(new_grid[lign][count]))
+                                grid[lign][column] = " "
                                 break
 
-    elif axis == "column":
-        
-        if direction == 1:
-
-            for column in range(len(grid)):
-                for lign in range(len(grid)):
-                    if grid[lign][column] != " ":
-                        for count in range(len(new_grid),0):
-                            if new_grid[count][column] == " ":
-                                new_grid[count][column] == grid[lign][column]
-                                break
-
-        elif direction == -1:
+    elif axis == "up":
 
             for column in range(len(grid)):
                 for lign in range(len(grid)):
                     if grid[lign][column] != " ":
                         for count in range(len(new_grid)):
                             if new_grid[count][column] == " ":
-                                new_grid[count][column] == grid[lign][column]
+                                new_grid[count][column] = grid[lign][column]
+                                grid[lign][column] = " "
+                                break
+                            elif new_grid[count][column] == grid[lign][column] and [count,column] not in already_merge:
+                                new_grid[count][column] = str(int(grid[lign][column]) + int(new_grid[count][column]))
+                                grid[lign][column] = " "
+                                break
+                                
+    elif axis == "down":
+            
+            for column in range(len(grid)):
+                for lign in range(len(grid)-1,-1,-1):
+                    if grid[lign][column] != " ":
+                        for count in range(len(new_grid)-1,-1,-1):
+                            if new_grid[count][column] == " ":
+                                new_grid[count][column] = grid[lign][column]
+                                grid[lign][column] = " "
+                                break
+                            elif new_grid[count][column] == grid[lign][column] and [count,column] not in already_merge:
+                                new_grid[count][column] = str(int(grid[lign][column]) + int(new_grid[count][column]))
+                                already_merge.append([count,column])
+                                grid[lign][column] = " "
                                 break
     return new_grid
+
+
+Game(Creategrid(4))
